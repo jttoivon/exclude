68ee93d86ca61018165e1c65c7577f240e8be7ba
Jarkko Toivonen <jarkko.toivonen@veripalvelu.fi>
2023-11-23 14:41:10 +0200

    Added another statistic

--- src/excluder.R
+++ src/excluder.R
@@ -1,29 +1,32 @@
 library(glue)
 
-init_excluder <- function(df, e_name) {
+init_excluder <- function(df, e_name, statistics = function(df) { list(count = nrow(df)) }) {
   if (is.null(.GlobalEnv[[".Excluder"]]))
     .GlobalEnv[[".Excluder"]] <- list()
   
-  .GlobalEnv[[".Excluder"]][[".current_e_name"]] <- e_name  # Name of the current excluder
-  .GlobalEnv[[".Excluder"]][[e_name]] <- list(count = nrow(df), log=c())
+  .GlobalEnv[[".Excluder"]][[".current_e_name"]] <- e_name  # Set the name of the current excluder
+  .GlobalEnv[[".Excluder"]][[e_name]] <- list(old=statistics(df), .log=c(), .statistics=statistics)
   invisible(df)
 }
 
 excluder <- function(df, 
-                     fmt="Old count was {old_count}, new count {new_count}, difference {old_count - new_count}",
+                     fmt="Old count was {old$count}, new count {new$count}, difference {old$count - new$count}",
                      e_name=NULL) {
   force(df)   # This is needed to prevent lazy evaluation, which breaks the side-effect
               # of setting a global variable.
   if (is.null(e_name))
     e_name <- .GlobalEnv[[".Excluder"]]$.current_e_name
-  old_count <- .GlobalEnv[[".Excluder"]][[e_name]]$count
-  old_log <- .GlobalEnv[[".Excluder"]][[e_name]]$log
-  new_count <- nrow(df)
-  diff <- old_count - new_count
-  cat(glue(fmt), "\n")
-  new_log <- append(old_log, glue(fmt))
-  .GlobalEnv[[".Excluder"]][[e_name]]$count <- nrow(df)
-  .GlobalEnv[[".Excluder"]][[e_name]]$log <- new_log
+  log <- .GlobalEnv[[".Excluder"]][[e_name]]$.log
+  statistics <- .GlobalEnv[[".Excluder"]][[e_name]]$.statistics
+  
+  .GlobalEnv[[".Excluder"]][[e_name]]$new <- statistics(df)
+
+  msg <- glue(fmt, .envir = .GlobalEnv[[".Excluder"]][[e_name]])  
+  cat(msg, "\n")
+  
+  log <- append(log, msg)
+  .GlobalEnv[[".Excluder"]][[e_name]]$old <- .GlobalEnv[[".Excluder"]][[e_name]]$new
+  .GlobalEnv[[".Excluder"]][[e_name]]$.log <- log
   
   df
 }
@@ -31,5 +34,5 @@ excluder <- function(df,
 dump_excluder <- function(filename, e_name=NULL) {
   if (is.null(e_name))
     e_name <- .GlobalEnv[[".Excluder"]]$.current_e_name
-  writeLines(.GlobalEnv[[".Excluder"]][[e_name]]$log, filename)
+  writeLines(.GlobalEnv[[".Excluder"]][[e_name]]$.log, filename)
 }
