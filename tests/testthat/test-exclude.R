7738a5015d9162f966485dc82999b1ddc6af43ae
Jarkko Toivonen <jarkko.toivonen@veripalvelu.fi>
2024-03-03 10:34:57 +0200

    Added a test

--- tests/testthat/test-exclude.R
+++ tests/testthat/test-exclude.R
@@ -5,3 +5,9 @@ test_that("exclusion works", {
   expect_equal(nrow(df), 1)
   expect_equal(nrow(edf), 2)
 })
+test_that("exclusion initialization works", {
+  df <- tibble::tibble(a=1:2)
+  init_exclude(df, "test")
+  e <- get_exclude()
+  expect_s3_class(e, "exclude")
+})
